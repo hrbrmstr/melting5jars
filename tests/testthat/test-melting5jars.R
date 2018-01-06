da57778425917b6f637d40432d1922767c00adc1
boB Rudis <bob@rud.is>
2018-01-06 12:48:54 -0500

    test

--- tests/testthat/test-melting5jars.R
+++ tests/testthat/test-melting5jars.R
@@ -1,4 +1,8 @@
-context("basic functionality")
-test_that("we can do something", {
+context("jar is recognized")
+test_that("jar is loaded, on classpath and classes can be instantiated", {
+
+  melting <- new(J("melting.Main"))
+
+  expect_equal(.jclass(melting), "melting.Main")
 
 })
